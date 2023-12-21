6f791b30efcebbf5ad5192a803e89cbb07a52b61
samanthay27 <144460844+samanthay27@users.noreply.github.com>
2023-12-21 09:53:23 -0500

    hjkg

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,6 +3,8 @@ import { DOMselectors } from './domselectors';
 //need to import domselectors 
 const URL ="https://gsi.fly.dev/";
 
+DOMselectors.container.addEventListener("")
+
 function makecard (){
     let a = DOMselectors.name.value;
     let b = DOMselectors.rarity.value;
@@ -10,28 +12,27 @@ function makecard (){
     let d = DOMselectors.weapon.value;
 
     DOMselectors.card.insertAdjacentElement("beforeend",
-    ' ')
+    `<div class = "card">
+    <h3> name: ${a}</h3>
+    <h3> rarity: ${b}</h3>
+    <h3> vision: ${c}</h3>
+    <h3> weapon: ${d}</h3>
+    </div>`);
 }
 
-document.querySelector('#app').innerHTML = `
-  <div>
-  <h2>hiiii</h2>
-   </div>
-`
-
-async function getData(URL) {
-    try {
-        const response = await fetch (URL);
-        //requesting a response REST API's 
-        if (response.status != 200) {
-            throw new Error (response.statusText);
-        }
-        const data = await response.json();
-        //converts response to JSON
-        document.querySelector("h1").textContent = data.content;
-    } catch (error) {
-        console.log(error, "gg")
-    }
-    //similar to ifelse
-}
-getData(URL);
+// async function getData(URL) {
+//     try {
+//         const response = await fetch (URL);
+//         //requesting a response REST API's 
+//         if (response.status != 200) {
+//             throw new Error (response.statusText);
+//         }
+//         const data = await response.json();
+//         //converts response to JSON
+//         document.querySelector("h1").textContent = data.content;
+//     } catch (error) {
+//         console.log(error, "gg")
+//     }
+//     //similar to ifelse
+// }
+// getData(URL);
